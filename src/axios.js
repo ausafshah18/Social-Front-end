c18b61a43122e06f7099ee9dd6d68e97d21300cd
Mohammad Ausaf Shah <81023295+ausafshah18@users.noreply.github.com>
2023-01-12 22:56:51 +0530

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const makeRequest = axios.create({
-    baseURL:"http://localhost:8800/api/",
+    baseURL:"https://stormy-teal-panther.cyclic.app",
     withCredentials:true, // because we want to give the access token to the backend server
 });
